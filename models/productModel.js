26acac96507db694ce93e485dfd4f7dd18aa9207
Raghavendra <kulalraghavendra4@gmail.com>
2023-08-06 15:07:58 +0530

    model created

---
+++ models/productModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const productSchema = mongoose.Schema(
+    {
+    id: {
+        type: mongoose.Schema.Types.ObjectId, // Assuming you want to use ObjectId for the ID field
+        default: mongoose.Types.ObjectId,
+        index: true, // Optional: Add an index to the ID for better performance
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter the name"],
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter the description"],
+    },
+},
+{
+timestamps:true
+}
+);
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
+
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const product = require("./productModel");
 
 
+const app = express();
+
+app.use(express.json());
 //routes
 
-app.get('/',(req,res)=>{
-    res.send('hello node api')
-})
-app.listen(3000,()=>{
-    console.log('Node api app is running on port 3000');
-})
+app.get("/", (req, res) => {
+  res.send("hello node api");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("hello blog");
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await product.create(req.body);
+    res.status(200).json({ message: error.message });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@crud.7tfhsnu.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+    app.listen(3000, () => {
+      console.log("Node api app is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
