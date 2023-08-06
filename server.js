580f72052946d5c04b2248f37698b2642f159d28
Raghavendra <kulalraghavendra4@gmail.com>
2023-08-06 16:20:20 +0530

    Put req. to change the current data

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Product = require("./models/productModel");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 //routes
 
 app.get("/", (req, res) => {
@@ -47,6 +48,26 @@ app.post('/product', async(req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+
+//update a product
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: `cannot find any user with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//de
 
 mongoose
   .connect(
